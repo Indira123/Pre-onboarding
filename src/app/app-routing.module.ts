eb7fc9a116ff6b14c7653388930a7e38f4eb2dfe
Vijaya Jiwalag Tiple <vijaya.tiple@yash.com>
2018-12-11 13:05:22 +0530

    Navigation changes of reports and dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { ReportsComponent } from './reports/reports.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'userDetail', component: UserDetailComponent }
+  { path: 'userDetail', component: UserDetailComponent },
+  { path: 'reports', component: ReportsComponent}
 ];
 @NgModule({
   exports: [ RouterModule ],
